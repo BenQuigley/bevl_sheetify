ce3b664911ca8251e1e481a147b3cb177ac1c0f0
Ben Quigley <benquigleyconsulting@gmail.com>
2017-10-10 11:28:25 -0400

    fixed issue recognizing courses

--- main.py
+++ main.py
@@ -20,18 +20,24 @@ def read_csv():
     return data
 
 class Audit:
+
     def __init__(self):
         self.requirements = []
 
-    def print_all(self):
+    def create_outfile(self, verbose=False):
         with open('outfile.csv', 'w') as outfile:
             writer = csv.writer(outfile)
             for r in self.requirements:
+                if verbose: print('requirement', r.name)
                 for c in r.concentrates:
+                    if verbose: print('  concentrate:', c.name)
                     for s in c.subrequirements:
+                        if verbose: print('    subrequirement:', s.name)
                         for course in s.courses:
-                            record = [r.name, c.name, s.name, course.full_name,
-                                      course.term_met, course.grade, course.credits]
+                            if verbose: print('      course:', course.name)
+                            record = [r.name, c.name, s.name,] #course.full_name,
+                                      #course.term_met, course.grade, course.credits]
+                            #if verbose: print(record)
                             writer.writerow(record)
 
 class Requirement:
@@ -61,15 +67,18 @@ class Course:
 def parse_lines(data):
     audit = Audit()
     requirement, concentrate, subrequirement, course = None, None, None, None
+    print('reading courses')
     for line in data:
+
         if line.find('C)') == 0:
             # Add any previous requirement.
             if requirement:
-                audit.append(requirement)
+                audit.requirements.append(requirement)
             # Process new requirement.
             name = line.split(':')[1].strip()
-            print("Found a new requirement:", name)
+            #print("Found a new requirement:", name)
             requirement = Requirement(name)
+
         elif line.find('C)') == 3:
             # Add any previous concentrates in audit's previous requirement.
             if concentrate:
@@ -77,33 +86,37 @@ def parse_lines(data):
             # Process new concentrate.
             name = line.split(':')[1].strip()
             concentrate = Concentrate(name)
+
         elif line.find('>') == 6:
             note_content = line.split('>')[1].strip()
             concentrate.note += ' '+note_content
+
         elif line.find('C)') == 6:
             # Add any previous courses in concentrate's previous subrequirement.
             if subrequirement:
                 concentrate.subrequirements.append(subrequirement)
             # Process new subrequirement.
-            name = line.split(':')[1].strip()
+            name = line.split(')')[1].strip()
             subrequirement = Subrequirement(name)
-        elif line[:10] == ''*10:
+
+        elif line[:10] == ' '*10:
             # Process new course.
             course = Course(full_name=line[10:45], term_met=line[46:52],
                             grade=line[55:57], credits=line[65:66])
+            print('new course:', course.full_name)
             subrequirement.courses.append(course)
-
     # Add final requirement.
-    audit.append(requirement)
+    audit.requirements.append(requirement)
 
-    # Make outfile.
-    audit.print_all()
+    return audit
+
 
 def main():
     print('Parsing data in csv file...')
     text = read_csv()
     print('Parsing records in data...')
-    parse_lines(text)
+    a = parse_lines(text)
+    a.create_outfile(verbose=False)
     print('Outfile created.')
 
 if __name__ == '__main__':
